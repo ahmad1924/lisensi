6512e3fa7cf0a6989ccc0f044b6b93487c7fa49e
ahmad1924 <51964497+ahmad1924@users.noreply.github.com>
2021-03-03 18:13:23 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -3,6 +3,7 @@ var twLSN = [
 'toko-whatsapp.blogspot.com',
 'jasa-design.web.id',
 'www.jasa-design.web.id',
+'https://mitrabundaindonesia.blogspot.com'
 ];
 
 var redirectUrl = 'http://bit.ly/toko-whatsapp';
